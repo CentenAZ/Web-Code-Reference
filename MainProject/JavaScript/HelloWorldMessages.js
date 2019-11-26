6f09c30e50f7981d0040615cfbc13b3ae21b2fc6
NickerFunk <vincent.nguyennf@gmail.com>
2019-11-26 12:48:05 -0700

    Initial Check-in

---
+++ MainProject/JavaScript/HelloWorldMessages.js
@@ -0,0 +1,5 @@
+// A box from the browser would pop up saying the message
+alert('Hello world!');
+
+// Console will output the message
+console.log('Hello World');
---
+++ MainProject/JavaScript/Variables.js
@@ -0,0 +1,13 @@
+let name = 'JavaScipt';
+console.log(name);
+// Variable name cannot be a reserved keyword (i.e. let, console, script, if, else)
+// Variable name cannot start with a number (i.e. 3name, 5script)
+// Names are also case-sensitive
+
+let firstName = 'John';
+let lastName = 'Doe';
+
+let interestRate = 0.125;
+console.log(interestRate);
+interestRate = 4;
+console.log(interestRate);
