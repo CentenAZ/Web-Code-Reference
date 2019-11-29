f7ec8fc08b6a40aca223795c89b682680d773d2d
NickerFunk <vincent.nguyennf@gmail.com>
2019-11-28 22:03:06 -0700

    Dynamic JavaScript

---
+++ MainProject/JavaScript/DynamicExample.js
@@ -0,0 +1,9 @@
+﻿function colorJS() {
+    var text = document.getElementById("inputInfo").value;
+    console.log("Value:  " + text);
+    var color = document.getElementById("colorSelection").value;
+    console.log(color);
+
+    document.getElementById("colorPrint").innerText = text;
+    document.getElementById("colorPrint").style.color = color;
+}
